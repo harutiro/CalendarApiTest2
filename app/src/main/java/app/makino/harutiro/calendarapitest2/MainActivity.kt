45d26fb23f1f834334049495ef82d4d8bf6d84ca
harutiro <harutiro2027@outlook.jp>
2021-05-03 02:08:42 +0900

    ようわからん2
    とりあえず、別スレッドで行うことで、HTTPの通信はできるようになったけど、それをウェブ認証するのは難しいことがわかった。
    Androidではデスクトップ、Webでのクラスが使えない場合があるっぽい。
    改めて、AndoroidでのOAuthのトークンの認証の仕方を調べることが必要そう

--- app/src/main/java/app/makino/harutiro/calendarapitest2/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/calendarapitest2/MainActivity.kt
@@ -1,9 +1,7 @@
 package app.makino.harutiro.calendarapitest2
 
-import CalendarQuickstart
 import android.content.Intent
-import android.graphics.Bitmap
-import android.os.AsyncTask
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -14,8 +12,29 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl
+import com.google.api.client.auth.oauth2.Credential
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
+import com.google.api.client.http.HttpTransport
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.DateTime
+import com.google.api.services.calendar.Calendar
+import com.google.api.services.calendar.CalendarScopes
+import com.google.api.services.calendar.model.Event
+import com.google.api.services.calendar.model.Events
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import java.io.FileNotFoundException
 import java.io.IOException
-import java.net.Socket
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.security.GeneralSecurityException
 import java.util.*
 
 
@@ -30,7 +49,13 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedList
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.button).setOnClickListener{
-            CalendarQuickstart.main()
+
+
+
+//            runBlocking(Dispatchers.IO){
+//                CalendarQuickstart.main()
+//            }
+
 
             onClick()
         }
@@ -89,67 +114,67 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedList
 
 }
 
-//object CalendarQuickstart {
-//    private const val APPLICATION_NAME = "Google Calendar API Java Quickstart"
-//    private val JSON_FACTORY: JsonFactory = JacksonFactory.getDefaultInstance()
-//    private const val TOKENS_DIRECTORY_PATH = "tokens"
-//
-//    // アクセスする権限のスコープ
-//    private val SCOPES: List<String> = Collections.singletonList(CalendarScopes.CALENDAR_READONLY)
-//
-//    // 認証情報
-//    private const val CREDENTIALS_FILE_PATH = "/credentials.json"
-//
-//    // 認証情報を取得する処理
-//    @Throws(IOException::class)
-//    private fun getCredentials(HTTP_TRANSPORT: HttpTransport): Credential {
-//        // Load client secrets.
-//        val `in` = CalendarQuickstart::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
-//                ?: throw FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH)
-//        val clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(`in`))
-//
-//        // 認証に利用するオブジェクトを作成
-//
-//        // Build flow and trigger user authorization request.
-//        val flow = GoogleAuthorizationCodeFlow.Builder(
-//                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-//                .setAccessType("offline")
-//                .build()
-//        // ローカルサーバを8888ポートで立てる
-//        val receiver = LocalServerReceiver.Builder().setPort(8888).build()
-//        return AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
-//    }
-//
-//    // メイン関数
-//    @Throws(IOException::class, GeneralSecurityException::class)
-//    @JvmStatic
-//    fun main() {
-//        val HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
-//        // カレンダーAPI呼び出し用のオブジェクトを作成。この時点でアクセストークンがないと認証が必要になる。
-//        val service: Calendar = Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-//                .setApplicationName(APPLICATION_NAME)
-//                .build()
-//
-//        // ここから先はGoogle Calendar APIを叩く処理
-//        val now = DateTime(System.currentTimeMillis())
-//        val events: Events = service.events().list("primary")
-//                .setMaxResults(10)
-//                .setTimeMin(now)
-//                .setOrderBy("startTime")
-//                .setSingleEvents(true)
-//                .execute()
-//        val items: List<Event> = events.getItems()
-//        if (items.isEmpty()) {
-//            println("No upcoming events found.")
-//        } else {
-//            println("Upcoming events")
-//            for (event in items) {
-//                var start: DateTime = event.getStart().getDateTime()
-//                if (start == null) {
-//                    start = event.getStart().getDate()
-//                }
-//                System.out.printf("%s (%s)\n", event.getSummary(), start)
-//            }
-//        }
-//    }
-//}
+object CalendarQuickstart {
+    private const val APPLICATION_NAME = "Google Calendar API Java Quickstart"
+    private val JSON_FACTORY: JsonFactory = JacksonFactory.getDefaultInstance()
+    private const val TOKENS_DIRECTORY_PATH = "tokens"
+
+    // アクセスする権限のスコープ
+    private val SCOPES: List<String> = Collections.singletonList(CalendarScopes.CALENDAR_READONLY)
+
+    // 認証情報
+    private const val CREDENTIALS_FILE_PATH = "/credentials.json"
+
+    // 認証情報を取得する処理
+    @Throws(IOException::class)
+    private fun getCredentials(HTTP_TRANSPORT: HttpTransport): Credential {
+        // Load client secrets.
+        val `in` = CalendarQuickstart::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
+                ?: throw FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH)
+        val clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(`in`))
+
+        // 認証に利用するオブジェクトを作成
+
+        // Build flow and trigger user authorization request.
+        val flow = GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setAccessType("offline")
+                .build()
+        // ローカルサーバを8888ポートで立てる
+        val receiver = LocalServerReceiver.Builder().setPort(8888).build()
+        return AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
+    }
+
+    // メイン関数
+    @Throws(IOException::class, GeneralSecurityException::class)
+    @JvmStatic
+    fun main() {
+        val HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
+        // カレンダーAPI呼び出し用のオブジェクトを作成。この時点でアクセストークンがないと認証が必要になる。
+        val service: Calendar = Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build()
+
+        // ここから先はGoogle Calendar APIを叩く処理
+        val now = DateTime(System.currentTimeMillis())
+        val events: Events = service.events().list("primary")
+                .setMaxResults(10)
+                .setTimeMin(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute()
+        val items: List<Event> = events.getItems()
+        if (items.isEmpty()) {
+            println("No upcoming events found.")
+        } else {
+            println("Upcoming events")
+            for (event in items) {
+                var start: DateTime = event.getStart().getDateTime()
+                if (start == null) {
+                    start = event.getStart().getDate()
+                }
+                System.out.printf("%s (%s)\n", event.getSummary(), start)
+            }
+        }
+    }
+}
